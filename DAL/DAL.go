6652b4f52a38e07acbaabe1721b61d8dceb4f426
hefju <hefju@live.cn>
2014-06-17 17:19:18 +0800

    xorm+sqlite3 突然间又得了
    神啊, 不要这样玩我啦, 上星期本想用xorm+sqlite3做个程序, 但是运行程序要33秒, 简直就是搞死人, 现在又突然间得了.
    究竟为什么呢

--- DAL/DAL.go
+++ DAL/DAL.go
@@ -3,6 +3,7 @@ package DAL
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-xorm/xorm"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -10,6 +11,63 @@ const (
 	driverName     = "sqlite3"
 	dataSourceName = "./odb.s3db"
 )
+
+//----------------------------xorm-------------------
+type User struct {
+	Id   int64  `xorm:autoincr`
+	Name string `xorm:"varchar(25) not null unique 'usr_name'"`
+}
+
+func CreateTableByXorm() {
+	engine, err := xorm.NewEngine(driverName, dataSourceName)
+	checkErr(err)
+	defer engine.Close()
+	engine.ShowSQL = true
+
+	engine.Sync(new(User))
+	// iamvoid(affected)
+
+}
+func getEngin() *xorm.Engine {
+	engine, err := xorm.NewEngine(driverName, dataSourceName)
+	checkErr(err)
+	return engine
+}
+
+func DeleteByXorm() {
+	sql := "delete from user"
+	_, err := getEngin().Exec(sql)
+	checkErr(err)
+}
+
+func InsertByXorm(name string) int64 {
+
+	user := new(User)
+	user.Name = name
+	affected, err := getEngin().Insert(user)
+	checkErr(err)
+	return affected
+}
+
+func FindByIDByXorm(uid int) []User {
+	users := make([]User, 0)
+	err := getEngin().Where("id=?", uid).Find(&users)
+	checkErr(err)
+	return users
+}
+
+func FindAllByXorm() []User {
+	users := make([]User, 0)
+	err := getEngin().Where("1=1").Find(&users)
+	checkErr(err)
+	return users
+}
+
+func iamvoid(x interface{}) {
+
+}
+
+//---------------------------------database/sql--------------
 
 func getDB() *sql.DB {
 	db, err := sql.Open(driverName, dataSourceName)
--- main.go
+++ main.go
@@ -10,73 +10,24 @@ import (
 	"time"
 )
 
+func displayAll() {
+	list := DAL.FindAllByXorm()
+	for i, v := range list {
+		fmt.Println("row:", i, " ", v.Id, "_", v.Name)
+	}
+}
 func main() {
-	rows := DAL.GetRows("SELECT * FROM " + "tmpTable")
 
-	var id int
-	var name string
-	for rows.Next() {
-		if err := rows.Scan(&id, &name); err == nil {
-			fmt.Println(id, " ", name)
-		}
+	//DAL.CreateTableByXorm()
+	//DAL.InsertByXorm("csc")
+	//DAL.DeleteByXorm()
+	displayAll()
 
-	}
-	// rows := QuerryData("tmpTable")
-
-	// var id string
-	// var name string
-	// for rows.Next() {
-	// 	if err := rows.Scan(&id, &name); err == nil {
-	// 		fmt.Println("id:", id, " name:", name)
-	// 	}
+	// list := DAL.FindByIDByXorm(2)
+	// for i, v := range list {
+	// 	fmt.Println("row:", i, " ", v.Id, "_", v.Name)
 	// }
-	//fmt.Println("什么垃圾go啊, 配置个运行环境都死人了")
-}
 
-func QuerryData(table string) *sql.Rows {
-	db, err := sql.Open("sqlite3", "./odb.s3db")
-	checkErr(err)
-
-	rows, err := db.Query("SELECT * FROM " + table)
-	checkErr(err)
-
-	db.Close()
-
-	return rows
-}
-
-func QueryPageData(index int) string {
-
-	rows := QuerryData("pageinfo")
-
-	var id int
-	var data string
-	page := ""
-
-	for rows.Next() {
-		if err := rows.Scan(&id, &data); err == nil && id == index {
-			page = data
-		}
-	}
-
-	return page
-}
-
-func QueryDateData() map[int]string {
-
-	rows := QuerryData("dateinfo")
-
-	var date int
-	var data string
-
-	memoryCache := make(map[int]string)
-	for rows.Next() {
-		if err := rows.Scan(&date, &data); err == nil {
-			memoryCache[date] = data
-		}
-	}
-
-	return memoryCache
 }
 
 type Task struct {
@@ -93,19 +44,3 @@ func checkErr(err error) {
 		fmt.Println("panic: " + err.Error())
 	}
 }
-
-// var x *xorm.Engine
-
-// func init() {
-// 	// 创建 ORM 引擎与数据库
-// 	var err error
-// 	x, err = xorm.NewEngine("sqlite3", "./bank.db")
-// 	if err != nil {
-// 		log.Fatalf("Fail to create engine: %v\n", err)
-// 	}
-
-// 	// 同步结构体与数据表
-// 	if err = x.Sync(new(Account)); err != nil {
-// 		log.Fatalf("Fail to sync database: %v\n", err)
-// 	}
-// }
